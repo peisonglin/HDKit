46823e9a1b38aae95407d29a023e2cc791534a6b
PSL <345455211@qq.com>
2021-12-03 12:47:44 +0800

    1、第一次提交

---
+++ Example/HDKit/HDViewController.h
@@ -0,0 +1,13 @@
+//
+//  HDViewController.h
+//  HDKit
+//
+//  Created by PSL on 12/03/2021.
+//  Copyright (c) 2021 PSL. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HDViewController : UIViewController
+
+@end
---
+++ Example/HDKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HDKit
+//
+//  Created by PSL on 12/03/2021.
+//  Copyright (c) 2021 PSL. All rights reserved.
+//
+
+@import UIKit;
+#import "HDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HDAppDelegate class]));
+    }
+}
